348c6d0ab3b9e28e9dd9e1af944b81c5b744acd0
KhpAgency <108800686+KhpAgency@users.noreply.github.com>
2023-04-12 13:13:19 +0200

    a

--- Dash-Board/main/js/function.js
+++ Dash-Board/main/js/function.js
@@ -1,13 +1,39 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  collectFormData();
+});
+
+
 function collectFormData() {
-    const form = document.querySelector('form');
-    const formData = new FormData(form);
-    const clientName = formData.get('clientName');
-    const logo = formData.get('logo');
-    const cover = formData.get('cover');
-  
-    console.log('Client Name:', clientName);
-    console.log('Logo:', logo);
-    console.log('Cover:', cover);
-  
-  }
-  
+  // const form = document.querySelector('form');
+  const formData = new FormData(form);
+  const clientName = formData.get('clientName');
+  const logo = formData.get('logo');
+  const cover = formData.get('cover');
+
+  // Get the file paths of the uploaded files
+  const logoPath = logo ? logo.name : '';
+  const coverPath = cover ? cover.name : '';
+
+  // console.log('Client Name:', clientName);
+  // console.log('Logo:', logoPath);
+  // console.log('Cover:', coverPath);
+
+  // You can then use the collected data to submit it to a server or perform other actions
+
+
+
+  axios.post('/addClient', {
+    clientName,
+    logo,
+    cover
+  })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+}
