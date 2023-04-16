2c7a18bc630a7116470fb8042497477d6e5cc5a5
Mohamed Khaled <khparchive@gmail.com>
2023-04-16 13:47:19 +0200

    mm

--- Dash-Board/main/js/3d.js
+++ Dash-Board/main/js/3d.js
@@ -1,3 +1,14 @@
-axios.get('http://localhost:3000/clients').then(function(e){
-    console.log(e.data).allData;
-    })
+
+async function getdata (){
+
+    
+  let data=  await axios.get('http://localhost:3000/clients').then(e=>console.log(e.data))
+    // console.log(data)
+    data.map(e=>e.data.name)
+    
+    // let option = document.getElementById('option').value
+    
+    // console.log(option);
+//    await data.map(e=>console.log(e))
+}
+getdata()
