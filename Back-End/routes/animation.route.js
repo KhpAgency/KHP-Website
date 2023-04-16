60583065bf35c2d79d38e036eea60f73c74efce3
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-04-16 04:55:24 +0200

    animation route

--- Back-End/routes/animation.route.js
+++ Back-End/routes/animation.route.js
@@ -1,34 +1,32 @@
 const app = require("express").Router();
 const clientModel = require("../model/client.model");
 const animationModel = require("../model/animation.model");
-const addProjectMiddleware = require("../middleware/addProject.middleware");
 
-
-app.post("/addAnimation",addProjectMiddleware, async (req, res) => {
+app.post("/addAnimation", async (req, res, next) => {
   let client = await clientModel.findOne({ name: req.body.clientName });
-  console.log(req.body);
 
-  //   if (client) {
-  //     const { clientName, videoLink } = req.body;
-  //     let id = client._id;
-  //     let animationExist = await animationModel.find({ name: clientName });
+  if (client) {
+    let id = client._id;
+    let animationExist = await animationModel.find({
+      name: req.body.clientName,
+    });
 
-  //     if (animationExist.length > 0) {
-  //       res.json({ message: "animation project already exists for this client name" });
-  //     } else {
-  //       let animationProject = await animationModel.insertMany({
-  //         name :clientName,
-  //         clientID:id,
-  //         videoLinks,
-  //       });
-  //       res.json({ message: "success", data: animationProject[0] });
-  //     }
-  //     next();
-  //   } else {
-  //     res.json({ message: "No client found" });
-  //   }
-  
-  res.json({ message: "test" });
+    if (animationExist.length > 0) {
+      res.json({
+        message: "animation project already exists for this client name",
+      });
+    } else {
+      let animationProject = await animationModel.insertMany({
+        name: req.body.clientName,
+        clientID: id,
+        videosLinks: req.body.animationUrl,
+      });
+      res.json({ message: "success", data: animationProject[0] });
+    }
+    next();
+  } else {
+    res.json({ message: "No client found" });
+  }
 });
 
 module.exports = app;
