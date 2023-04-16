ed4d9bde74ab001f7a2b6b2288f8c76faa3fc6b7
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-04-16 14:30:04 +0200

    m

--- Dash-Board/main/js/3d.js
+++ Dash-Board/main/js/3d.js
@@ -3,13 +3,11 @@ async function getdata (){
 
     
   let {data}=  await axios.get('http://localhost:3000/clients')
-    console.log(data)
+
   let names=  data.map((item)=>(
     `<option value=${item.name} id="option"> ${item.name} </option>`
   ))
-
   document.getElementById('clients').innerHTML=names;
 
-//   console.log(names);
 }
 getdata()
