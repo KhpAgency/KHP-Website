16b53a838b948c1de7b51e47fa0fe8f322facc62
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-04-30 14:13:56 +0300

    v

--- Front-End/js/4d.js
+++ Front-End/js/4d.js
@@ -1,10 +1,12 @@
 async function get3dprojects() {
+  let MagdyUrl= 'C:/Users/Lenovo/Documents/GitHub/KHP-Website/Back-End'
+  let FaresUrl= 'C:/Users/khpar/OneDrive/Documents/GitHub/KHP-Website/Back-End'
 
   let { data } = await axios.get("http://localhost:3000/all4d");
   let x = data.map(item => item)
   console.log(x);
 
-  let project = data.map((item) => `<img data-aos="fade-down" data-aos-duration="500" style="margin-top: 80px;" src="C:/Users/khpar/OneDrive/Documents/GitHub/KHP-Website/Back-End/${item.clientID.logo}" alt=""
+  let project = data.map((item) => `<img data-aos="fade-down" data-aos-duration="500" style="margin-top: 80px;" src="${MagdyUrl}/${item.clientID.logo}" alt=""
         class="cscale2">
   
         <div data-aos="fade-right" data-aos-delay="500" data-aos-duration="500"
@@ -13,13 +15,13 @@ async function get3dprojects() {
 
         ${item.threeDphotos.map((items) =>(`<div class="swiper-slide">
 
-        <img src="C:/Users/khpar/OneDrive/Documents/GitHub/KHP-Website/Back-End/${items.path}" alt="">
+        <img src="${MagdyUrl}/${items.path}" alt="">
 
       </div>`))}
         
         ${item.videoLink &&`<div class="swiper-slide">
         <iframe width="950" height="490"
-        src="https://www.youtube.com/embed/T4kkoCMyCCY?loop=1&controls=1&autoplay=0&mute=1"
+        src="${item.videoLink}"
         title="YouTube video player" frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen></iframe>
@@ -55,62 +57,3 @@ async function get3dprojects() {
 }
 
 get3dprojects();
-
-
-
-
-
-
-
-
-// async function get3dprojects() {
-
-//   let { data } = await axios.get("http://localhost:3000/all3d");
-//   let x = data.map(item => item)
-//   console.log(x);
-//   // let x = threeDphotos[0].map(items => items)
-//   // let path = x.map(item => item.path)
-//   // console.log(path);
-//   // console.log(threeDphotos.map((item=>item[0]).map(e =>e.path)));
-//   // console.log(threeDphotos[0].map(items => items.path));
-
-
-
-//   let project = data.map((item) => `<img data-aos="fade-down" data-aos-duration="500" style="margin-top: 80px;" src="C:/Users/khpar/OneDrive/Documents/GitHub/KHP-Website/Back-End/${item.clientID.logo}" alt=""
-//         class="cscale2">
-  
-//         <div data-aos="fade-right" data-aos-delay="500" data-aos-duration="500"
-//         class="bodyy swiper-container mySwiper width90" style="width: 60%;margin-top: 40px;">
-//         <div class="swiper-wrapper">
-
-//         ${item.threeDphotos.map((items) =>(`<div class="swiper-slide">
-
-//         <img src="C:/Users/khpar/OneDrive/Documents/GitHub/KHP-Website/Back-End/${items.path}" alt="">
-
-//       </div>`))}
-        
-//         ${item.videoLink &&`<div class="swiper-slide">
-//         <iframe width="950" height="490"
-//         src="https://www.youtube.com/embed/T4kkoCMyCCY?loop=1&controls=1&autoplay=0&mute=1"
-//         title="YouTube video player" frameborder="0"
-//         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-//         allowfullscreen></iframe>
-//         </div>`}
-        
-  
-//         </div>
-//         <div class="swiper-button-next arrows"></div>
-//         <div class="swiper-button-prev arrows"></div>
-//         <div class="swiper-pagination"></div>
-//         </div>`
-//   );
-//   document.getElementById("3dprojects").innerHTML = project;
-// }
-// get3dprojects();
-
-// ${item.threeDphotos.map((item2) => {
-//   `<img class="lazyload" data-src="C:/Users/Lenovo/Documents/GitHub/KHP-Website/Back-End/${item2.path}" alt="">`;
-// })}
-
-
-// <img class="lazyload" data-src="images/visual/3D/elsafwa/El Safwa-01.jpg" alt="">
