61347f9e91f8860748c4cab923da4d0accc11b42
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-04-30 12:23:01 +0300

    4d

--- Dash-Board/main/js/4d.js
+++ Dash-Board/main/js/4d.js
@@ -61,26 +61,3 @@ function collectFormData() {
       console.log("");
     });
 }
-
-//   <img data-aos="fade-down" data-aos-duration="500" style="margin-top: 80px;" src="images/clients/safwa1.png" alt=""
-//   class="cscale2">
-
-// <div data-aos="fade-right" data-aos-delay="500" data-aos-duration="500"
-//   class="bodyy swiper-container mySwiper width90" style="width: 60%;margin-top: 40px;">
-//   <div class="swiper-wrapper">
-//     <div class="swiper-slide">
-//       <img class="lazyload" data-src="images/visual/3D/elsafwa/El Safwa-01.jpg" alt="">
-//     </div>
-//     <div class="swiper-slide">
-//       <iframe width="950" height="490"
-//         src="https://www.youtube.com/embed/T4kkoCMyCCY?loop=1&controls=1&autoplay=0&mute=1"
-//         title="YouTube video player" frameborder="0"
-//         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-//         allowfullscreen></iframe>
-//     </div>
-
-//   </div>
-//   <div class="swiper-button-next arrows"></div>
-//   <div class="swiper-button-prev arrows"></div>
-//   <div class="swiper-pagination"></div>
-// </div>
