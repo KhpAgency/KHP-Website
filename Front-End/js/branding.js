d8d41ff972e6e8289c61c70d7a3ad895b528841e
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-05-04 15:47:21 +0300

    v

--- Front-End/js/branding.js
+++ Front-End/js/branding.js
@@ -1,35 +1,18 @@
+let url = 'https://khp-api.onrender.com/';
 async function getbranding() {
-    let url = 'https://khp-api.onrender.com/';
   
     let { data } = await axios.get(`${url}allProjects`);
     console.log(data);
   
     let slider1 = data.map((item) => `<div class="owl-carousel">
-    <div class="item">
-      <button class="btn2" onclick="filterSelection('${item.clientName}')"><a title="${item.clientName}" href="#div${item.clientName}"><img
-            class="imgscale" src="${url}${(item.clientID.logo.replace("uploads\\", ""))}" alt=""></a></button>
-    </div>
-  </div>`)
+      <div class="item">
+        <button class="btn2" onclick="filterSelection('${item.clientName}')"><a title="${item.clientName}" href="#div${item.clientName}"><img
+              class="imgscale" src="${url}${(item.clientID.logo.replace("uploads\\", ""))}" alt=""></a></button>
+      </div>
+    </div>`).join('')
   
     document.getElementById("slider1").innerHTML = slider1;
-  
-    // Initialize Swiper slider
-    var swiper = new Swiper(".mySwiper", {
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 4500,
-        disableOnInteraction: false
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-      }
-    });
+
   
   }
   
